6ee1571b62fe82efa8d8cda393036532ed9fb010
Grant Smith <grant.smith@tufts.edu>
2021-03-09 11:21:45 -0500

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -2,8 +2,6 @@
 # Ballistic Simulator with drag to calculate initial conditions to get a ball to desired
 # x-coordinate if initial velocity is fixed
 import math
-import time
-import sys 
 import matplotlib.pyplot as plt
 import numpy as np
   
@@ -12,13 +10,12 @@ import numpy as np
 
 # Ballistic equations
 #Initial Conditions
-desi_distance = 100
+desi_distance = 2
 g = 9.81
-vel_0 = 100
+vel_0 = 10
 
 
 def calc_distance(vel_0, theta):
-    theta_t = .1
     U_0 = vel_0*math.cos(math.radians(theta))
     V_0 = vel_0*math.sin(math.radians(theta))
     t = 2*V_0/g
